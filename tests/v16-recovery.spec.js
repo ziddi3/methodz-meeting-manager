0c0f1924ba453e94121515e762af2cebd388bd8e
Ziddi C.J. <zidthesunkid@gmail.com>
2026-07-25 18:02:12 -0600

    test: advance recovery shell expectation to v1.6.6

--- tests/v16-recovery.spec.js
+++ tests/v16-recovery.spec.js
@@ -24,7 +24,7 @@ test(`${RECOVERY_PREFIX} recovery readiness panel and guarded import architectur
 
   expect(state).toEqual({
     schema: "1.6.0",
-    shell: "1.6.5",
+    shell: "1.6.6",
     core: "function",
     recovery: "function",
     restoreGuarded: true,
